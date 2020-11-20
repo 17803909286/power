4caf0a1fe9c2d1494b75f489241eae4e1dcf36f7
17803909286 <1017762553@qq.com>
2020-11-20 18:30:17 +0800

    图片适配

--- app/src/main/java/com/power/home/ui/activity/GuideOriginatorActivity.java
+++ app/src/main/java/com/power/home/ui/activity/GuideOriginatorActivity.java
@@ -3,10 +3,12 @@ package com.power.home.ui.activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.graphics.Point;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Handler;
 import android.os.Message;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewTreeObserver;
@@ -18,10 +20,13 @@ import com.power.home.R;
 import com.power.home.common.util.DensityUtil;
 import com.power.home.di.component.AppComponent;
 
+import butterknife.BindView;
 import me.jessyan.autosize.AutoSize;
 import me.jessyan.autosize.internal.CustomAdapt;
 
 public class GuideOriginatorActivity extends BaseActivity   {
+
+
 
     private Handler hideHandler = new Handler(){
         @Override
@@ -43,7 +48,7 @@ public class GuideOriginatorActivity extends BaseActivity   {
 
     @Override
     public boolean init() {
-        this.scaleImage(findViewById(R.id.iv_head),R.mipmap.dlxs_app);
+        this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         hideHandler.sendEmptyMessageDelayed(0,3000);
 
         return true;
@@ -52,42 +57,6 @@ public class GuideOriginatorActivity extends BaseActivity   {
     @Override
     public void setListener() {
 
-    }
-    private void scaleImage(View view,int sourceId){
-
-        Point point = new Point();
-        this.getWindow().getWindowManager().getDefaultDisplay().getSize(point);
-        Bitmap resourceBitmap = BitmapFactory.decodeResource(this.getResources(),sourceId);
-        if (resourceBitmap == null) {
-            return;
-        }
-
-
-        int bitmapScaledHeight = Math.round(resourceBitmap.getHeight() * point.x * 1.0f / resourceBitmap.getWidth());
-
-        final Bitmap scaledBitmap = Bitmap.createScaledBitmap(resourceBitmap, point.x, bitmapScaledHeight, false);
-
-       view.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
-           @Override
-           public boolean onPreDraw() {
-               if(scaledBitmap.isRecycled()){
-                   return true;
-               }
-               int viewHeight = view.getMeasuredHeight();
-               Log.i("offsetheightview",String.valueOf(viewHeight));
-               int offset = (scaledBitmap.getHeight() - viewHeight) / 2;
-               Log.i("offsetheight",String.valueOf(offset));
-               Bitmap finallyBitmap = Bitmap.createBitmap(scaledBitmap, 0, offset, scaledBitmap.getWidth(),
-                       scaledBitmap.getHeight() - offset * 2 );
-               if (!finallyBitmap.equals(scaledBitmap)) {//如果返回的不是原图，则对原图进行回收
-                   scaledBitmap.recycle();
-                   System.gc();
-               }
-               ((ImageView)view).setImageBitmap(finallyBitmap);
-               return true;
-
-           }
-       });
     }
 
     @Override
